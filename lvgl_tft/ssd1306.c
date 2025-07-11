aa69f5ccaca0eda915ac809bb6cae305c0d86f57
Valeriy Chudnikov <memric@yandex.ru>
2025-07-11 22:11:35 +0300

    Change assert to ESP_ERROR_CHECK

--- lvgl_tft/ssd1306.c
+++ lvgl_tft/ssd1306.c
@@ -127,8 +127,7 @@ void ssd1306_init(void)
         OLED_CMD_DISPLAY_ON
     };
 
-    uint8_t err = send_data(NULL, conf, sizeof(conf));
-    assert(0 == err);
+    ESP_ERROR_CHECK(send_data(NULL, conf, sizeof(conf)));
 }
 
 void ssd1306_set_px_cb(lv_disp_drv_t * disp_drv, uint8_t * buf, lv_coord_t buf_w, lv_coord_t x, lv_coord_t y,
@@ -162,10 +161,8 @@ void ssd1306_flush(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t 
         row2,
     };
 
-    uint8_t err = send_data(disp_drv, conf, sizeof(conf));
-    assert(0 == err);
-    err = send_pixels(disp_drv, color_p, OLED_COLUMNS * (1 + row2 - row1));
-    assert(0 == err);
+    ESP_ERROR_CHECK(send_data(disp_drv, conf, sizeof(conf)));
+    ESP_ERROR_CHECK(send_pixels(disp_drv, color_p, OLED_COLUMNS * (1 + row2 - row1)));
 
     lv_disp_flush_ready(disp_drv);
 }
@@ -188,8 +185,7 @@ void ssd1306_sleep_in(void)
         OLED_CMD_DISPLAY_OFF
     };
 
-    uint8_t err = send_data(NULL, conf, sizeof(conf));
-    assert(0 == err);
+    ESP_ERROR_CHECK(send_data(NULL, conf, sizeof(conf)));
 }
 
 void ssd1306_sleep_out(void)
@@ -199,8 +195,7 @@ void ssd1306_sleep_out(void)
         OLED_CMD_DISPLAY_ON
     };
 
-    uint8_t err = send_data(NULL, conf, sizeof(conf));
-    assert(0 == err);
+    ESP_ERROR_CHECK(send_data(NULL, conf, sizeof(conf)));
 }
 
 /**********************
